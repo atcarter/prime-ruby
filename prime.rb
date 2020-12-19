f7d41718ae8eac88e0ad71cdcd8d20d71c5b03e4
Antonio <atcarter005@gmail.com>
2020-12-19 11:13:26 -0600

    commit tree 7651adba260ee0c77129c961165287687cbe6790

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+def prime?(num)
+    bool = true
+    if bool == 2 
+        return bool 
+    end
+
+    if num <= 1
+        return false
+    end
+
+    array = (2..num).to_a
+    array.each do |number|
+        remainder = num % number 
+        if remainder == 0 && number != num
+            bool = false
+        end
+    end
+    
+    bool
+end
+
